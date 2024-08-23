847af8751f34aa133b3f2fd71ba4d6ebe19085be
jacky <jackyiron2003@gmail.com>
2024-08-24 03:56:38 +0800

    Changes

--- adviseBySVM.py
+++ adviseBySVM.py
@@ -110,15 +110,29 @@ def analyze_stock(stock_name, stock_code, stock_type, revenue_per_share_yoy, pri
     price_difference = estimated_price - latest_close_price
     price_diff_percentage = price_difference / latest_close_price * 100
 
-    if abs(price_diff_percentage) > 60 and epst4q[-1] > 0 :
-        color = 'darkred' if latest_close_price > estimated_price else 'lightseagreen'
-        action = '强力卖出' if latest_close_price > estimated_price else '强力买入'
-    elif 30 <= abs(price_diff_percentage) <= 60 and epst4q[-1] > 0:
-        color = 'red' if latest_close_price > estimated_price else 'green'
-        action = '卖出' if latest_close_price > estimated_price else '买入'
+    # 根据价格差异和 EPST4Q 的值确定颜色和操作
+    if price_diff_percentage > 60:
+        if epst4q[-1] > 0:
+            color = 'lightseagreen'
+            action = '强力买入'
+        else:
+            color = 'darkred'
+            action = '强力卖出'
+    elif 30 <= price_diff_percentage <= 60:
+        if epst4q[-1] > 0:
+            color = 'green'
+            action = '买入'
+        else:
+            color = 'red'
+            action = '卖出'
     else:
         color = 'black'
         action = ''
+
+    result_message = (f'<span style="color: {color};">{stock_name} {stock_code} ({stock_type}) - '
+                      f'实际股价: {latest_close_price:.2f}, 推算股价: {estimated_price:.2f} ({price_diff_percentage:.2f}%) {action} '
+                      f'MSE: {final_mse:.2f} </span><br>')
+
 
     result_message = (f'<span style="color: {color};">{stock_name} {stock_code} ({stock_type}) - '
                       f'实际股价: {latest_close_price:.2f}, 推算股价: {estimated_price:.2f} ({price_diff_percentage:.2f}%) {action} '
